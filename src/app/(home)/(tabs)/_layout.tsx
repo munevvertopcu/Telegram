1522d6e8239ee62ff0fccdb0bea637c962ef3390
munevvertopcu <munevvertopcu98@gmail.com>
2025-06-21 10:49:39 +0300

    Setup Expo Router

---
+++ src/app/(home)/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { Tabs } from "expo-router";
+
+export default function TabsNavigator() {
+    return <Tabs>
+        <Tabs.Screen
+            name="index"
+            options={{
+                title: "Chats",
+                tabBarIcon: ({ size, color }) => (
+                    <FontAwesome5 name="home" size={size} color={color} />
+                ),
+            }}
+        />
+        <Tabs.Screen
+            name="profile"
+            options={{
+                title: "Profile",
+                tabBarIcon: ({ size, color }) => (
+                    <FontAwesome5 name="user-alt" size={size} color={color} />
+                ),
+            }}
+        />
+    </Tabs>
+}
